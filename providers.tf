267d6ee5f028899e09397f390c494b01fc1b5432
Ponkanagaraj <anishraj1990@hotmail.com>
2026-01-01 15:05:55 +0530

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIAS2ZLVM6SCA5VIG2R"
+    secret_key = "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"
+  }
